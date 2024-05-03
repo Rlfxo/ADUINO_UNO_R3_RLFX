8d036397729bd48eb1d428848483b1a77f212c38
Rlfxo <yskl7137@gmail.com>
2024-05-03 21:53:26 +0900

    Moisture Sensor Test Code

--- MoistureSensor/DFROBOT_Capacitive_Soil_Moisture_Sensor_V1/DFROBOT_Capacitive_Soil_Moisture_Sensor_V1.ino
+++ MoistureSensor/DFROBOT_Capacitive_Soil_Moisture_Sensor_V1/DFROBOT_Capacitive_Soil_Moisture_Sensor_V1.ino
@@ -1,12 +1,27 @@
+const int AirValue = 505;
+const int WaterValue = 260;
+int intervals = (AirValue - WaterValue)/3;
+int soilMoistureValue = 0;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int val;
-  val = analogRead(0);  //connect sensor to Analog 0
-  Serial.println(val);  //print the value to serial port
+  soilMoistureValue = analogRead(A0);
+
+  if(soilMoistureValue > WaterValue && soilMoistureValue < (WaterValue + intervals))
+  {
+    Serial.println("Very Wet");
+  }
+  else if(soilMoistureValue > (WaterValue + intervals) && soilMoistureValue < (AirValue - intervals))
+  {
+    Serial.println("Wet");
+  }
+  else if(soilMoistureValue < AirValue && soilMoistureValue > (AirValue - intervals))
+  {
+    Serial.println("Dry");
+  }
+  
   delay(100);
 }
